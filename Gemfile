733aa1220e3e63bdaf07fb21a2b3a022ee8f67b9
Friedaz <rheganandar@gmail.com>
2018-10-10 16:09:00 +0700

    Modified assets

--- Gemfile
+++ Gemfile
@@ -89,3 +89,4 @@ gem 'lightbox-bootstrap-rails', '5.1.0.1'
 
 gem 'jquery-rails', '~> 4.0', '>= 4.0.5'
 gem 'mailboxer', github: 'mailboxer/mailboxer'
+gem 'bootstrap-sass'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require lightbox-bootstrap
+
 //= require activestorage
 //= require jquery3
 //= require jquery_ujs
