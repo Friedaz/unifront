ca3246e2ae1bd108f126f295c654f14996c501c4
Friedaz <rheganandar@gmail.com>
2018-10-10 15:22:42 +0700

    config

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Unifront
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
     # config/application.rb
     config.assets.initialize_on_precompile = false
+
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
